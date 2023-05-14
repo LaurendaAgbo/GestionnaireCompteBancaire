2ca666df44807dfc436bcd5dd935550983ba5f76
AGBO <alaurenda2@gmail.com>
2023-05-14 18:39:49 +0100

    Add project files.

---
+++ Client.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionnaireCompteBancaire
+{
+    //Classe Client
+    class Client
+    {
+        // Variables
+        private string nom;
+        private string numeroCompte;
+        double soldeCourant;
+        double soldeEpargne;
+
+        //Constructeur
+        public Client(string nom, string numeroCompte)
+        {
+            this.nom = nom;
+            this.numeroCompte = numeroCompte;
+            this.soldeCourant = 0.0;
+            this.soldeEpargne = 0.0;
+        }
+
+        //Accesseurs et mutateurs
+        public string Nom
+        {
+            get
+            {
+                return this.nom;
+            }
+            set
+            {
+                this.nom = value;
+            }
+        }
+
+        public string NumeroCompte
+        {
+            get
+            {
+                return this.numeroCompte;
+            }
+            set
+            {
+                this.numeroCompte = value;
+            }
+        }
+
+        public double SoldeCourant
+        {
+            get
+            {
+                return this.soldeCourant;
+            }
+            set
+            {
+                this.soldeCourant = value;
+            }
+        }
+
+        public double SoldeEpargne
+        {
+            get
+            {
+                return this.soldeEpargne;
+            }
+            set
+            {
+                this.soldeEpargne = value;
+            }
+        }
+
+        //Méthodes
+        // Afficher les informations du compte client
+        public string AfficherInformations()
+        {
+            string informations = ("Nom du client: " + this.Nom + "\nNuméro de compte : " + this.NumeroCompte);
+            //Console.WriteLine("Nom du client : " + this.Nom);
+            //Console.WriteLine("Numéro de compte : " + this.NumeroCompte);
+            return informations;
+        }
+
+        // Afficher le solde du compte courant
+        public void AfficherSoldeCourant()
+        {
+            Console.WriteLine("Solde du compte courant : " + this.SoldeCourant + "$");
+        }
+
+        // Afficher le solde du compte épargne
+        public void AfficherSoldeEpargne()
+        {
+            Console.WriteLine("Solde du compte épargne : " + this.SoldeEpargne + "$");
+        }
+
+        // Faire un dépôt sur le compte client quelque soit le type
+        public double DepotCompte(string typeCompte)
+        {
+            Console.WriteLine("Quel montant souhaitez-vous déposer ?");
+            double depot = Double.Parse(Console.ReadLine());
+
+            if (typeCompte == "CD")
+            {
+                this.SoldeCourant += depot;
+            }
+            if (typeCompte == "ED")
+            {
+                this.SoldeEpargne += depot;
+            }
+
+            Console.WriteLine("Vous avez déposé : " + depot + "$");
+            return depot;
+        }
+
+        // Faire un retrait sur le compte client quelque soit le type
+        public double RetraitCompte(string typeCompte)
+        {
+            Console.WriteLine("Quel montant souhaitez-vous retirer ?");
+            double retrait = Double.Parse(Console.ReadLine());
+
+            if (typeCompte == "CD")
+            {
+                this.SoldeCourant -= retrait;
+            }
+            if (typeCompte == "ED")
+            {
+                this.SoldeEpargne -= retrait;
+            }
+
+            Console.WriteLine("Vous avez retiré : " + retrait + "$");
+            return retrait;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GestionnaireCompteBancaire
+{
+    /// <summary>
+    /// Exercice d'application du cours "Apprendre à programmer en C#" sur OpenClassrooms
+    /// Application de gestion de comptes bancaires (courant et épargne) des clients d'une banque
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Client client = new Client("Jane Doe", "0987654");
+            IList<string> listeDesTransactions = new List<string>();
+            string choixMenu = "";
+
+            string infosClient = client.AfficherInformations();
+            double depot = 0;
+            double retrait = 0;
+            string entreeDepotCourant = "";
+            string entreeDepotEpargne = "";
+            string entreeRetraitCourant = "";
+            string entreeRetraitEpargne = "";
+            listeDesTransactions.Add(infosClient);
+
+            do
+            {
+                //Mettre à jour la liste des transactions lors d'un dépôt
+                if (depot > 0)
+                {
+                    if (choixMenu.ToUpper() == "CD")
+                    {
+                        entreeDepotCourant = ("Transaction : Dépôt de " + depot + "$ sur le compte courant à " + DateTime.Now.ToString() + ". Solde actuel : " + client.SoldeCourant + "$");
+                        listeDesTransactions.Add(entreeDepotCourant);
+                    }
+                    if (choixMenu.ToUpper() == "ED")
+                    {
+                        entreeDepotEpargne = ("Transaction : Dépôt de " + depot + "$ sur le compte épargne à " + DateTime.Now.ToString() + ". Solde actuel : " + client.SoldeEpargne + "$");
+                        listeDesTransactions.Add(entreeDepotEpargne);
+                    }
+                    depot = 0;
+                }
+
+                ////Mettre à jour la liste des transactions lors d'un retrait
+                if (retrait > 0)
+                {
+                    if (choixMenu.ToUpper() == "CR")
+                    {
+                        entreeRetraitCourant = ("Transaction : Retrait de " + retrait + "$ du compte courant à " + DateTime.Now.ToString() + ". Solde actuel : " + client.SoldeCourant + "$");
+                        listeDesTransactions.Add(entreeRetraitCourant);
+                    }
+                    if (choixMenu.ToUpper() == "ER")
+                    {
+                        entreeRetraitEpargne = ("Transaction : Retrait de " + retrait + "$ du compte épargne à " + DateTime.Now.ToString() + ". Solde actuel : " + client.SoldeEpargne + "$");
+                        listeDesTransactions.Add(entreeRetraitEpargne);
+                    }
+                    retrait = 0;
+                }
+
+                // Afficher le menu
+                Menu();
+                // Choisir une des options
+                choixMenu = Console.ReadLine();
+
+                switch (choixMenu.ToUpper())
+                {
+                    case "I":  // Voir les informations sur le titulaire du compte
+                        Console.WriteLine(client.AfficherInformations());
+                        break;
+                    case "CS":  // Consulter le solde courant
+                        client.AfficherSoldeCourant();
+                        break;
+                    case "CD":  // Effectuer un dépôt sur le compte courant
+                        depot = client.DepotCompte(choixMenu.ToUpper());
+                        break;
+                    case "CR":  // Effectuer un retrait sur le compte courant
+                        retrait = client.RetraitCompte(choixMenu.ToUpper());
+                        break;
+                    case "ES":  // Consulter le solde épargne
+                        client.AfficherSoldeEpargne();
+                        break;
+                    case "ED":  // Effectuer un dépôt sur le compte épargne
+                        depot = client.DepotCompte(choixMenu.ToUpper());
+                        break;
+                    case "ER":  // Effectuer un retrait sur le compte épargne
+                        retrait = client.RetraitCompte(choixMenu.ToUpper());
+                        break;
+                    case "X":  // Quitter
+                        Console.Clear();
+                        EcrireFichier(listeDesTransactions, "transactions");
+                        Console.WriteLine("Merci et à bientôt !");
+                        //Environment.Exit(0);
+                        break;
+                }
+            } while (choixMenu.ToUpper() != "X");
+
+        }
+
+        // Afficher le menu
+        public static void Menu()
+        {
+            Console.WriteLine("Appuyez sur Entrée pour afficher le menu.");
+            Console.ReadLine();
+            Console.WriteLine("Veuillez sélectionner une option ci-dessous :");
+            Console.WriteLine("[I] Afficher les informations sur le titulaire du compte");
+            Console.WriteLine("[CS] Compte courant - Consulter le solde");
+            Console.WriteLine("[CD] Compte courant - Déposer des fonds");
+            Console.WriteLine("[CR] Compte courant - Retirer des fonds");
+            Console.WriteLine("[ES] Compte épargne - Consulter le solde");
+            Console.WriteLine("ED[] Compte épargne - Déposer des fonds");
+            Console.WriteLine("[ER] Compte épargne - Retirer des fonds");
+            Console.WriteLine("[X] Quitter");
+        }
+
+        // Écrire toutes les transactions du compte courant et du compte épargne dans un fichier texte
+        public static void EcrireFichier(IList<string> transactions, string nomFichier)
+        {
+            try
+            {
+                using (StreamWriter resume = new StreamWriter(nomFichier + ".txt", true))
+                {
+                    foreach (string transaction in transactions)
+                    {
+                        resume.WriteLine(transaction);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Impossible d'écrire dans le fichier " + nomFichier + e.ToString());
+            }
+        }
+    }
+}
